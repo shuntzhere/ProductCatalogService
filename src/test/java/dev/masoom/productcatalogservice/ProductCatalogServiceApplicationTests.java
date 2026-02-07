de8971c014c58ce0b6c549cdf98f39df71f205f9
Masoom Kumar <masoom.kr04@gmail.com>
2026-02-07 14:58:43 +0530

    Add initial test package generated by Spring Boot

---
+++ src/test/java/dev/masoom/productcatalogservice/ProductCatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.masoom.productcatalogservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductCatalogServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
